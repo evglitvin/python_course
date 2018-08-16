f3161224f0e4abb6fc1958300e7a25fe0d44a5f6
Evgeniy Lytvyn <ext-ievgen.lytvyn@here.com>
2018-08-16 14:45:14 +0300

    MINOR: added lesson 7 and adv

--- lesson1/types_testing.py
+++ lesson1/types_testing.py
@@ -1,7 +1,17 @@
 
 # default arguments
+import math
+
+ttt = 0
 
 def func(l=[]): # wrong: default value is mutable
+    global ttt
+
+    for temp_var in xrange(10):
+        pass
+
+    print temp_var == 9
+    ttt = 5
     in_list = l or []
     in_list.append(1)
     return in_list
@@ -23,16 +33,26 @@ print s ^ s1
 b = l
 print l, b
 idx = 0
+idx_del = -1
 while idx < len(b):
     if b[idx] % 2 == 0:
         del b[idx]
+        idx_del = idx
     else:
         idx += 1
+if idx_del == -1:
+    print idx_del
 print b
 
 # list-comprehentions
-l = (item for item in read_lines if item % 2 == 0)
-f = filter(None, l)
+l = (item for item in read_lines if my_filter(x))
+
+
+def my_filter(x):
+    return math.floor(x) != 0
+
+f = filter(my_filter, [0.5435, 2.4325,  5.43242, 43.252])
+
 l1 = xrange(10 ** 10)
 
 # too large list
@@ -53,8 +73,14 @@ def my_xrange(start=None, stop=None, step=1):
         yield x0
         x0 += step
 
+from operator import *
 
 def my_range(start=None, stop=None, step=1):
+    lt_fn = lt
+    and_(lt(start, stop), gt(step, 0))
+    and_(lt(start, stop), gt(step, 0))
+    if (start < stop and step > 0) or (start > stop and step < 0):
+        raise ValueError("step should go from start to end")
     n = stop
     x0 = 0
     if not stop:
---
+++ lesson7/adv_lesson1.py
@@ -0,0 +1,70 @@
+def test_func(a):
+    # use a for reading
+    f = a
+
+    # creating new var
+    if expr:
+        a = 1
+
+    try:
+        open('fjireogfe')
+        g = 5
+    except IOError as e:
+        pass
+
+    if expr:
+        g = 1
+    else:
+        g = 1
+    print g
+
+
+set_1 = {0, 128, 1, 3, 256}
+for item in set_1:
+    print item
+
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other[0], self.y + other[1])
+
+
+class Rect(object):
+    def __init__(self, (x, y)=(0, 0), (width, height)=(100, 100)):
+        self._pos = Point(x, y)
+        self._size = width, height
+
+    @property
+    def pos(self):
+        return self._pos
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def center(self):
+        return (self._pos.x + self._size[0]) / 2, (self._pos.y + self._size[1]) / 2
+
+    @property
+    def bounding_box(self):
+        return self._pos, self._pos + self._size, \
+               self._pos + (self._pos.x, self._size[1]), \
+               self._pos + (self._size[0], self._pos.y)
+
+    def point_in_rect(self, point):
+        return self._pos.x < point.x and self._pos.y < point.y and \
+               self._pos.x + self._size[0] > point.x and self._pos.y + self._size[1] > point.y
+
+    def overlap(self, other):
+        for point in other.bounding_box:
+            if self.point_in_rect(point):
+                return True
+        return False
+
+
+print Rect((0, 0), (200, 200)).overlap(Rect((200, 200), (300, 300)))
