ececbf00794500d39cf1ccfb20c9520273e34487
lytvyn <ext-ievgen.lytvyn@here.com>
2019-04-05 11:23:16 +0300

    MINOR: addedd lesson29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,5 +1,11 @@
+from StringIO import StringIO
+from cStringIO import StringIO as ctr
+from zipfile import ZipFile
+
 from lesson20.adv import get_bits_for_number, get_bits_count1
 
+import struct
+from socket import inet_aton
 
 def playing_with_ip():
     ip = '123.45.46.245'
@@ -13,8 +19,9 @@ def playing_with_ip():
     print bin(ipaddr)
 
     # forming mask
-    mask = (1 << (32 - 16)) - 1
-
+    0/24
+    mask = ((1 << (32 - 24)) - 1) << 8
+    print bin(mask)
     print bin(mask & ipaddr), mask & ipaddr
 
 
@@ -36,4 +43,27 @@ def diff_of_b_s_values():
     print minmask, bin(minmask)
     return mask - minmask
 
-diff_of_b_s_values()
+
+DASHED_STYLE = 0x1
+DOTTED_STYLE = 0x2
+SOLID_STYLE =  0x4
+
+def line(point1, point2, style=0):
+    if DASHED_STYLE & style:
+        print "DASHED_STYLE"
+    if DOTTED_STYLE & style:
+        print "DOTTED_STYLE"
+    if SOLID_STYLE & style:
+        print "SOLID_STYLE"
+
+line(0,0, DOTTED_STYLE | DASHED_STYLE)
+
+f = StringIO()
+
+zf = ZipFile(f, mode='w')
+zf.write('__init__.py')
+zf.close()
+
+print f.getvalue()
+
+# playing_with_ip()
